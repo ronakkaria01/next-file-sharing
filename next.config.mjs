fe92605d4006f5c75aa814cb93d8dc33aaa87993
Ronak Karia <ronak@sozodesign.co.uk>
2024-05-12 13:39:02 +0530

    add docker ip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                `${process.env.NEXT_PUBLIC_DOCKER_IP}`
+            ]
+        }
+    }
+};
 
 export default nextConfig;
